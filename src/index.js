735ea1c75333d4eef2339de42f6cd0f3b861af18
qus0in <qus0in@gmail.com>
2025-07-25 15:53:43 +0900

    feat(api): API 키 인증 및 계정 엔드포인트 추가
    - `express.json()` 미들웨어를 추가하여 JSON 요청 본문을 처리할 수 있도록 개선합니다.
    - 모든 API 요청에 대해 API 키를 확인하는 인증 미들웨어를 추가했습니다.
    - 계정 조회를 위한 `/accounts` 엔드포인트를 신규 추가했습니다.
    
    BREAKING CHANGE: API 요청 시 `Authorization` 헤더에 API 키를 포함해야 합니다.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,22 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
+app.use(express.json());
+
+app.get("/", (_, res) => {
   res.send("HEALTH CHECK 😀");
+});
+
+// API Key CHECK
+app.use((req, res, next) => {
+  if (req.headers["authorization"] !== `${process.env.API_KEY}`) {
+    return res.status(401).send("Unauthorized");
+  }
+  next();
+});
+
+app.get("/accounts", (req, res) => {
+  res.send("ACCOUNTS ENDPOINT 😀");
 });
 
 app.listen(port, () => {
