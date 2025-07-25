8f7a7196408dfeeb01f733fe7f7da5c39b2e53b9
qus0in <qus0in@gmail.com>
2025-07-25 11:59:07 +0900

    chore(pm2): 클러스터 모드 설정 제거

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,6 @@ module.exports = {
       script: "src/index.js",
       node_args: "--env-file=.env.prod",
       name: "sachiel",
-      instances: 2,
-      exec_mode: "cluster",
       max_memory_restart: "1G",
       autorestart: true,
       watch: false,
